432c268f4c3f82dc6521bedf0b05a87654a44f1a
takehaya <hayatake396@gmail.com>
2015-08-10 23:38:38 +0900

    Initial Commit

---
+++ 冷蔵庫帳/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  冷蔵庫帳
+//
+//  Created by 早坂彪流 on 2015/08/10.
+//  Copyright © 2015年 早坂彪流. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
